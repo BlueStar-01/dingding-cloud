e8ff1253d6171278997d3d3eaefebdcbd31e5bfa
碧蓝小新星 <2745672197@qq.com>
2025-01-12 15:49:12 +0800

    交易依赖解决。等待运行实验。

--- dingding-trade/src/main/java/com/BlueStar/trade/TradeApplication.java
+++ dingding-trade/src/main/java/com/BlueStar/trade/TradeApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
 @EnableFeignClients(basePackages = "com.BlueStar.api.client")
+@SpringBootApplication
+//@MapperScan(basePackages = "com.BlueStar.trade.mapper")
 public class TradeApplication {
     public static void main(String[] args) {
         SpringApplication.run(TradeApplication.class, args);
